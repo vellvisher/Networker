9002994021e6ef8cf1cd614113ff1c625b45eafb
vellvisher <vaarnan@gmail.com>
2013-02-23 15:34:34 +0800

    Added button to go to send activity

--- src/vsp/networker/MainActivity.java
+++ src/vsp/networker/MainActivity.java
@@ -1,8 +1,10 @@
 package vsp.networker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void sendPageButton(View view) {
+		Intent intent = new Intent(this, SendPage.class);
+		startActivity(intent);
 	}
 
 }
