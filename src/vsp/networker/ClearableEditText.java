241f9fb0cb7fdaaf4823631500f6738a0753d220
Poornima Muthukumar <poo.dav@gmail.com>
2013-02-24 02:17:50 +0800

    plus button added

--- src/vsp/networker/ClearableEditText.java
+++ src/vsp/networker/ClearableEditText.java
@@ -12,86 +12,86 @@ import android.widget.RelativeLayout;
 
 public class ClearableEditText extends RelativeLayout
 {
-LayoutInflater inflater = null;
-EditText edit_text;
-ImageButton btn_clear;
+	LayoutInflater inflater = null;
+	EditText edit_text;
+	ImageButton btn_clear;
 
 public ClearableEditText(Context context, AttributeSet attrs, int defStyle)
 {
-super(context, attrs, defStyle);
-initViews();
+	super(context, attrs, defStyle);
+	initViews();
 }
 
 public ClearableEditText(Context context, AttributeSet attrs)
 {
-super(context, attrs);
-initViews();
+	super(context, attrs);
+	initViews();
 
 }
 
 public ClearableEditText(Context context)
 {
-super(context);
-initViews();
+	super(context);
+	initViews();
 }
 
 void initViews()
 {
-inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-inflater.inflate(R.layout.clearable_edit_text, this, true);
-edit_text = (EditText) findViewById(R.id.clearable_edit);
-btn_clear = (ImageButton) findViewById(R.id.imageButton1);
-btn_clear.setVisibility(RelativeLayout.INVISIBLE);
-clearText();
-showHideClearButton();
+	inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	inflater.inflate(R.layout.clearable_edit_text, this, true);
+	edit_text = (EditText) findViewById(R.id.clearable_edit);
+	btn_clear = (ImageButton) findViewById(R.id.clearable_button_clear);
+	btn_clear.setVisibility(RelativeLayout.INVISIBLE);
+	clearText();
+	showHideClearButton();
 }
 
 void clearText()
 {
-btn_clear.setOnClickListener(new OnClickListener()
-{
-@Override
-public void onClick(View v)
-{
-// TODO Auto-generated method stub
-edit_text.setText("");
-}
-});
+	btn_clear.setOnClickListener(new OnClickListener()
+	{
+	@Override
+	public void onClick(View v)
+	{
+		// TODO Auto-generated method stub
+		edit_text.setText("");
+		}
+	});
 }
 
 void showHideClearButton()
 {
 edit_text.addTextChangedListener(new TextWatcher()
 {
-
-@Override
-public void onTextChanged(CharSequence s, int start, int before, int count)
-{
-// TODO Auto-generated method stub
-if (s.length() > 0)
-btn_clear.setVisibility(RelativeLayout.VISIBLE);
-else
-btn_clear.setVisibility(RelativeLayout.INVISIBLE);
-}
-@Override
-public void beforeTextChanged(CharSequence s, int start, int count, int after)
-{
-// TODO Auto-generated method stub
-
-}
+	
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count)
+	{
+		// TODO Auto-generated method stub
+		if (s.length() > 0)
+		btn_clear.setVisibility(RelativeLayout.VISIBLE);
+		else
+		btn_clear.setVisibility(RelativeLayout.INVISIBLE);
+	}
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after)
+	{
+	// TODO Auto-generated method stub
+	
+	}
 
 @Override
 public void afterTextChanged(Editable s)
 {
-// TODO Auto-generated method stub
-
+	// TODO Auto-generated method stub
+	
 }
-});
+	});
 }
 
 public Editable getText()
 {
-Editable text = edit_text.getText();
-return text;
+	Editable text = edit_text.getText();
+	return text;
 }
 }
--- src/vsp/networker/Contacts.java
+++ src/vsp/networker/Contacts.java
@@ -11,8 +11,9 @@ public class Contacts extends Activity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts);
-		 
 	}
+
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
